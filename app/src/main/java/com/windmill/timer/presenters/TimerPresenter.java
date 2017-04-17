c31fdfbcec651b0c65270193b4d5da868d3fb854
Regina Garaeva <garaeva.regina9@gmail.com>
2017-04-17 23:36:33 +0500

    Inject timer views to TimerPresenter and create timer.xml.

--- app/src/main/java/com/windmill/timer/presenters/TimerPresenter.java
+++ app/src/main/java/com/windmill/timer/presenters/TimerPresenter.java
@@ -1,19 +1,25 @@
 package com.windmill.timer.presenters;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
+import butterknife.BindView;
+import com.windmill.timer.R;
 import com.windmill.timer.model.Timer;
 
 public class TimerPresenter implements Presenter<Timer> {
+    @BindView(R.id.timer_time) TextView time;
     private View view;
     private Timer timer;
 
-    public TimerPresenter(View view) {
-        this.view = view;
+    public TimerPresenter(LayoutInflater inflater) {
+        this.view = inflater.inflate(R.layout.timer, null);
     }
 
     @Override
     public void set(Timer timer) {
         this.timer = timer;
+        time.setText(timer.getTime().toString());
     }
 
     @Override
